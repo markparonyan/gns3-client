52ab8813190e750614054caf78d0fc0cea59a0ad
Mark Paronyan <markparonian@gmail.com>
2025-03-11 13:13:30 +0000

    feat(nodes_api): add remaining endpoints

--- gns3client/api/nodes.py
+++ gns3client/api/nodes.py
@@ -269,4 +269,115 @@ class NodesAPI(BaseAPI):
             "reload_all_nodes_v3_projects_project_id_nodes_reload_post",
             "NodesApi->reload_all_nodes_v3_projects_project_id_nodes_reload_post",
             path_params={"project_id": project_id}
+        )
+        
+    def isolate(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "isolate_node_v3_projects_project_id_nodes_node_id_isolate_post",
+            "NodesApi->isolate_node_v3_projects_project_id_nodes_node_id_isolate_post",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def unisolate(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "unisolate_node_v3_projects_project_id_nodes_node_id_unisolate_post",
+            "NodesApi->unisolate_node_v3_projects_project_id_nodes_node_id_unisolate_post",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def links(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "get_node_links_v3_projects_project_id_nodes_node_id_links_get",
+            "NodesApi->get_node_links_v3_projects_project_id_nodes_node_id_links_get",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def get_file(self, access_token, project_id, node_id, file_path):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "get_file_v3_projects_project_id_nodes_node_id_files_file_path_get",
+            "NodesApi->get_file_v3_projects_project_id_nodes_node_id_files_file_path_get",
+            path_params={"project_id": project_id, "node_id": node_id, "file_path": file_path}
+        )
+        
+    def post_file(self, access_token, project_id, node_id, file_path, body):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "post_file_v3_projects_project_id_nodes_node_id_files_file_path_post",
+            "NodesApi->post_file_v3_projects_project_id_nodes_node_id_files_file_path_post",
+            path_params={"project_id": project_id, "node_id": node_id, "file_path": file_path},
+            body=body
+        )
+        
+    def reset_console_all(self, access_token, project_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "reset_console_all_nodes_v3_projects_project_id_nodes_console_reset_post",
+            "NodesApi->reset_console_all_nodes_v3_projects_project_id_nodes_console_reset_post",
+            path_params={"project_id": project_id}
+        )
+        
+    def console_reset(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "console_reset_v3_projects_project_id_nodes_node_id_console_reset_post",
+            "NodesApi->console_reset_v3_projects_project_id_nodes_node_id_console_reset_post",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def auto_idlepc(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "auto_idlepc_v3_projects_project_id_nodes_node_id_dynamips_auto_idlepc_get",
+            "NodesApi->auto_idlepc_v3_projects_project_id_nodes_node_id_dynamips_auto_idlepc_get",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def idlepc_proposals(self, access_token, project_id, node_id):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "idlepc_proposals_v3_projects_project_id_nodes_node_id_dynamips_idlepc_proposals_get",
+            "NodesApi->idlepc_proposals_v3_projects_project_id_nodes_node_id_dynamips_idlepc_proposals_get",
+            path_params={"project_id": project_id, "node_id": node_id}
+        )
+        
+    def create_disk_image(self, access_token, project_id, node_id, disk_name, body=None):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "create_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_post",
+            "NodesApi->create_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_post",
+            path_params={"project_id": project_id, "node_id": node_id, "disk_name": disk_name},
+            body=body
+        )
+        
+    def update_disk_image(self, access_token, project_id, node_id, disk_name, body):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "update_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_put",
+            "NodesApi->update_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_put",
+            path_params={"project_id": project_id, "node_id": node_id, "disk_name": disk_name},
+            body=body
+        )
+        
+    def delete_disk_image(self, access_token, project_id, node_id, disk_name):
+        return self._call_api(
+            access_token,
+            NodesApi,
+            "delete_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_delete",
+            "NodesApi->delete_disk_image_v3_projects_project_id_nodes_node_id_qemu_disk_image_disk_name_delete",
+            path_params={"project_id": project_id, "node_id": node_id, "disk_name": disk_name}
         ) 
